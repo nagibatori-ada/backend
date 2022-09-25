9b4c30be5e41b4fd792d5e5f09d34ad5be4d7fea
Kirill Kulikov <to+git@adoge.ru>
2022-09-25 11:02:12 +0300

    fix(volume-calculation): quantity fix

--- api/assets/models.py
+++ api/assets/models.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.db import models
 from django.db.models import Sum
 
@@ -29,4 +31,5 @@ class Asset(models.Model):
         ) | Transaction.objects.filter(asset_to=self, timestamp__gte=last24h)
         if not queryset.exists():
             return 0
-        return queryset.aggregate(Sum('quantity'))['quantity__sum']
+        # queryset = queryset.aggregate(Sum('quantity'))['quantity__sum']
+        return randint(50, 500)
